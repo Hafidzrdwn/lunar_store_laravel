0d7a6943f2cb371c43c0bb0ef9abccac68c2e386
Hafidzrdwn <hafidz10032005@gmail.com>
2025-06-05 03:39:54 +0700

    feat: add new client page [aboutus]

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\Client;
 
 Route::get('/', Client\Landing::class)->name('landing');
+Route::get('/aboutus', Client\Aboutus::class)->name('aboutus');
