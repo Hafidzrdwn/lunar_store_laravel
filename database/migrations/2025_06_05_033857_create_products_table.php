5e58e3e75925d05182bbb39c72d674fef1d62fa5
Hafidzrdwn <hafidz10032005@gmail.com>
2025-06-06 08:25:10 +0700

    feat: setup the database and table [model & migration]

---
+++ database/migrations/2025_06_05_033857_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('app_name', 200);
+            $table->text('description');
+            $table->string('cover_img')->nullable();
+            $table->string('notes', 150)->nullable();
+            $table->boolean('has_type')->default(false);
+            $table->boolean('ready_stock')->default(true);
+            $table->foreignId('category_id')->constrained('product_categories')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
